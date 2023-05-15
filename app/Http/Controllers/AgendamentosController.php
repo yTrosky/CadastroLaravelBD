7b4d4c38ae7c0aa4e81d443f680a231766b7c6ce
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2023-05-15 17:33:02 -0300

    Update AgendamentosController.php

--- app/Http/Controllers/AgendamentosController.php
+++ app/Http/Controllers/AgendamentosController.php
@@ -7,11 +7,10 @@ use App\Models\Agendamentos;
 
 class AgendamentosController extends Controller
 {
-    // Método post para validar e enviar os dados
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            // Variável para armazenar os dados enviados pelos campos
+
             'nome' => 'required|max:255',
             'telefone' => 'required|max:20',
             'origem' => 'required|max:255',
@@ -20,7 +19,6 @@ class AgendamentosController extends Controller
 
         ]);
 
-            // Enviando os dados para o banco de dados
         $agendamento = new Agendamentos();
         $agendamento->nome = $request->nome;
         $agendamento->telefone = $request->telefone;
@@ -29,18 +27,14 @@ class AgendamentosController extends Controller
         $agendamento->observacao = $request->observacao;
         $agendamento->save();
 
-            // Retornando para o formulário de cadastro
         return redirect()->route('index')
             ->with('success', 'Agendamento criado com sucesso!');
     }
 
-    // Método get para mostrar os dados na tabela
     public function show(){
 
-        // Recupera todos os agendamentos do banco de dados
         $tableagenda = Agendamentos::all();
         
-        // Retorna a view 'consultar' com os agendamentos recuperados
         return view('consulta', ['agendamentos' => $tableagenda]);
     }
 }
